0e280525ea245d0b56537dc5d24db224d4b9d3c3
sznsky <sznsky1988@gmail.com>
2026-06-23 23:29:22 +0800

    存款和取款

--- contracts/src/pool/LendingPool.sol
+++ contracts/src/pool/LendingPool.sol
@@ -30,6 +30,112 @@ contract LendingPool is ReentrancyGuard {
     // 价格预言机,用来读取weth,usdc的价格，immutable=部署时候配置一次，之后不再改变，节省gas
     IPriceOracle public immutable oracle;
 
+    // 存款总额
+    uint256 public totalShares;
+    // 用户存款份额
+    mapping (address => uint256) public shares;
+
+    // 借款仓位
+    struct Position {
+        // 抵押品(ETH)
+        uint256 collateral;
+        // 债务(USDC)
+        uint256 debt;
+    }
+    // 用户借款仓位
+    mapping (address => Position) public positions;
+    // 借款总额
+    uint256 public totalDebt;
+
+    // 事件
+    event Supply(address indexed user, uint256 amount, uint256 sharesMinted);
+    event Withdraw(address indexed user, uint256 amount, uint256 sharesBurned);
+    event DepositCollateral(address indexed user, uint256 amount);
+    event WithdrawCollateral(address indexed user, uint256 amount);
+    event Borrow(address indexed user, uint256 amount);
+    event Repay(address indexed user, uint256 amount);
+    event Liquidate(
+        address indexed liquidator,
+        address indexed borrower,
+        uint256 debtRepaid,
+        uint256 collateralSeized
+    );
+
+    // 构造函数
+    constructor(address _usdc, address _weth, address _oracle) {
+        usdc = IERC20(_usdc);
+        weth = IERC20(_weth);
+        oracle = IPriceOracle(_oracle);
+    }
+
+    // 池子总资产=池内udsc现金+全部应收的债务
+    function totalAssets() public view returns (uint256) {
+        return usdc.balanceOf(address(this)) + weth.balanceOf(address(this));
+    }
+
+    // 健康因子HF=抵押品的价值*清算阈值 / 债务价值。
+    function getHealthFactor(address user) public view returns (uint256) {
+        Position memory position = positions[user];
+        uint256 collateralValue = position.collateral * oracle.getPrice(address(weth)) / HF_PRECISION;
+        uint256 debtValue = position.debt * oracle.getPrice(address(usdc)) / HF_PRECISION;
+        return collateralValue * LIQUIDATION_THRESHOLD_BPS / debtValue;
+    }
+
+    // phase1: 存款和取款
+    function supply(uint256 amount) public nonReentrant {
+        // 存款金额大于0
+        require(amount > 0, "Amount must be greater than 0");
+        // 存款前池子总资产
+        uint256 assetsBefore = totalAssets();
+        // 从用户账户转出USDC到池子
+        usdc.transferFrom(msg.sender, address(this), amount);
+        // 计算用户存款份额,首次存款时，份额与存款金额相等，之后按比例分配
+        uint256 sharesToMint = totalShares == 0? amount: (amount * totalShares) / assetsBefore; 
+        // 更新用户存款份额
+        shares[msg.sender] += sharesToMint;
+        // 更新池子总份额
+        totalShares += sharesToMint;
+        emit Supply(msg.sender, amount, sharesToMint);
+    }
+
+    // 取款
+    function withdraw(uint256 amount) external nonReentrant {
+        // 取款金额大于0
+        require(amount > 0, "zero amount");
+        // 用户存款份额
+        uint256 userShares = shares[msg.sender];
+        // 用户存款份额大于0
+        require(userShares > 0, "no shares");
+        // 池子总资产
+        uint256 assets = totalAssets();
+        // 计算用户需要销毁的份额
+        uint256 sharesToBurn = (amount * totalShares) / assets;
+        // 用户需要销毁的份额大于0且小于等于用户存款份额
+        require(sharesToBurn > 0 && sharesToBurn <= userShares, "insufficient shares");
+        // 池子内USDC现金
+        uint256 cash = usdc.balanceOf(address(this));
+        // 取款金额小于等于池子内USDC现金
+        require(amount <= cash, "insufficient liquidity");
+        // 更新用户存款份额
+        shares[msg.sender] -= sharesToBurn;
+        // 更新池子总份额
+        totalShares -= sharesToBurn;
+        // 从池子转出USDC到用户账户
+        usdc.safeTransfer(msg.sender, amount);
+        emit Withdraw(msg.sender, amount, sharesToBurn);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
